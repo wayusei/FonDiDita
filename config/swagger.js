ebef3afa886682307da0b940c51c6a7d553e5c77
angel-pm <angel.pime.99@gmail.com>
2022-10-08 23:58:22 -0500

    se arreglaron conflicto al bajar cambios

--- config/swagger.js
+++ config/swagger.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e82b5128e57dedc501ad777662b40f95292c29b
 // configure the Swagger UI
 const options = {
     definition: {
@@ -12,20 +8,12 @@ const options = {
             description:
                 "Este es un CRUD API de la F2 de Bedu hecho con Express y documentado con Swagger",
             contact: {
-<<<<<<< HEAD
                 name: "Equipo-16 - GitHub repository",
-=======
-                name: "Equipo-16",
->>>>>>> 1e82b5128e57dedc501ad777662b40f95292c29b
-                url: "https://github.com/wayusei/FonDiDita",
-                email: ""
+                url: "https://github.com/wayusei/FonDiDita"
             },
         }
     },
-<<<<<<< HEAD
     server: "localhost:3000/",
-=======
->>>>>>> 1e82b5128e57dedc501ad777662b40f95292c29b
     apis: ["./routes/*.js"],
     components: {
         securitySchemes: {
@@ -35,45 +23,8 @@ const options = {
                 bearerFormat: "JWT",
                 value: "Bearer <JWT token here>"
             }
-        }/* ,
-        schemas: {
-            products: {
-                type: "object",
-                required: ["id","name","price","description","image","thumbnail","category","seller_id"],
-                properties: {
-                    id: {
-                        type: "integer"
-                    },
-                    name: {
-                        type: "string"
-                    },
-                    price: {
-                        type: "integer"
-                    },
-                    description: {
-                        type: "string"
-                    },
-                    image: {
-                        type: "string"
-                    },
-                    thumbnail: {
-                        type: "string"
-                    },
-                    category: {
-                        type: "integer"
-                    },
-                    seller_id: {
-                        type: "integer"
-                    }
-                }
-            }
-        } */
+        }
     }
 }
 
-<<<<<<< HEAD
-module.exports = options
-=======
-module.exports = options
-
->>>>>>> 1e82b5128e57dedc501ad777662b40f95292c29b
+module.exports = options;
